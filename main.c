17116caf7e4a1f8ef7cd8248833ae778f2e8e850
Thomas <thomas@MacBook-Pro-de-Thomas.local>
2025-01-09 16:43:18 +0100

    commit

--- main.c
+++ main.c
@@ -6,17 +6,69 @@
 /*   By: thomas <thomas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 17:04:40 by thomas            #+#    #+#             */
-/*   Updated: 2025/01/07 14:58:17 by thomas           ###   ########.fr       */
+/*   Updated: 2025/01/09 16:40:45 by thomas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	main(int argc, char **argv)
+int	find_path(char **env)
 {
-	if (argc > 4)
+	char	*path;
+	int		i;
+
+	path = "PATH";
+	i = 0;
+	while (env[i] != NULL)
 	{
-		if (access(argv[1], F_OK) == -1)
-			ft_printf("No such file or directory : %s", argv[1]);
-	}	
+		if (ft_strncmp(path, env[i], 4) ==  0)
+			return (i);
+		i++;
+	}
+	return (-1);
 }
+
+void	exec_path(char **argv, char **env)
+{
+	char	**path;
+	char	**cmd;
+	int		i;
+
+	cmd = ft_split(argv[2], ' ');
+	cmd[0] = ft_strjoin("/", cmd[0]);
+	path = ft_split(env[find_path(env)], ':');
+	i = 0;
+	while (path[i] != NULL)
+	{
+		path[i] = ft_strjoin(path[i], cmd[0]);
+		if (access(path[i], F_OK | X_OK) == 0)
+		{
+			execve(path[i], cmd, env);
+		}
+		i++;
+	}
+}
+
+int	main(int argc, char **argv, char **env)
+{
+	// if (argc > 4)
+	// {
+	// 	char **tab = ft_split(argv[2], ' ');
+	// 	while ( != NULL)
+	// 	{
+	// 		if (access(argv[1], F_OK) == -1)
+	// 			ft_printf("No such file or directory : %s", argv[1]);
+	// 		ft_printf("%d", execve(tab[0], tab, env));
+	// 		i++;
+	// 	}
+	// }
+	int	std_in;
+	std_in = open(argv[1], O_RDONLY);
+	dup2(std_in, 0);
+	(void)argc;
+	exec_path(argv, env);
+}
+
+/* donc faire l'executable avec execve faire un str join avec la premiere parti de la commande
+sur le split des commande et savoir si on a acces a ce dossier et qu'on est la version X et qu'il
+existe*/
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: thomas <thomas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/21 11:16:39 by tmillot           #+#    #+#             */
-/*   Updated: 2025/01/07 09:44:21 by thomas           ###   ########.fr       */
+/*   Updated: 2025/01/09 16:40:09 by thomas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,8 @@
 # include "./ft_printf/ft_printf.h"
 # include <stdio.h>
 # include <unistd.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
 
 #endif
