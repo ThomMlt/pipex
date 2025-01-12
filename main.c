26bf970ec46c1e386668408c094153da72c9f966
Thomas <thomas@MacBook-Pro-de-Thomas.local>
2025-01-13 00:08:49 +0100

    comit

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: thomas <thomas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 17:04:40 by thomas            #+#    #+#             */
-/*   Updated: 2025/01/12 23:29:21 by thomas           ###   ########.fr       */
+/*   Updated: 2025/01/13 00:05:02 by thomas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,25 +28,45 @@ int	find_path(char **env)
 	return (-1);
 }
 
+void ft_free_split(char **split)
+{
+    int	i;
+
+	i = 0;
+    while (split[i])
+    {
+        free(split[i]);
+        i++;
+    }
+    free(split);
+}
+
 void	exec_path(char *argv, char **env)
 {
 	char	**path;
 	char	**cmd;
+	char	*tmp;
 	int		i;
 
 	cmd = ft_split(argv, ' ');
-	cmd[0] = ft_strjoin("/", cmd[0]);
+	tmp = ft_strjoin("/", cmd[0]);
+	free(cmd[0]);
+	cmd[0] = tmp;
 	path = ft_split(env[find_path(env)], ':');
 	i = 0;
 	while (path[i] != NULL)
 	{
-		path[i] = ft_strjoin(path[i], cmd[0]);
+		tmp = ft_strjoin(path[i], cmd[0]);
 		if (access(path[i], F_OK | X_OK) == 0)
 		{
-			execve(path[i], cmd, env);
+			execve(tmp, cmd, env);
+			free(tmp);
 		}
+		free(tmp);
 		i++;
 	}
+	ft_free_split(path);
+	ft_free_split(cmd);
 }
 
 void	processus_1(char *in_file, char *cmd, int pipefd[2], char **env)
@@ -103,61 +123,3 @@ int	main(int argc, char **argv, char **env)
     waitpid(pid1, NULL, 0);
     waitpid(pid2, NULL, 0);
 }
-
-	// int		std_in = 0;
-	// int 	std_out = 0;
-	// int		pipefd[2];
-	// pid_t	pid1;
-	// pid_t	pid2;
-	
-	// if (argc != 5)
-	// 	exit(1);
-	// pid1 = fork();
-	// pid2 = fork();
-	// pipefd[0] = std_in;
-	// pipefd[1] = std_out;
-	// pipe(pipefd);
-	// if (pid1 == 0)
-	// {
-	// 	std_in = open(argv[1], O_RDONLY);
-	// 	dup2(std_in, 0);
-	// 	dup2(pipefd[1], 1);
-	// 	exec_path(argv[2], env);
-	// 	close(pipefd[0]);
-	// 	close(std_in);
-	// }
-	// if (pid2 == 0)
-	// {
-	// 	std_out = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	// 	dup2(pipefd[0], 0);
-	// 	dup2(std_out, 1);
-	// 	close(pipefd[1]);
-    //     close(std_out);
-    //     exec_path(argv[3], env);
-	// }
-	// close(pipefd[0]);
-    // close(pipefd[1]);
-    // waitpid(pid1, NULL, 0);
-    // waitpid(pid2, NULL, 0);
-
-	
-	// if (argc > 4)
-	// {
-	// 	if (pid == 0)
-	// 	{
-	// 		if (access(argv[1], F_OK) == 0)
-	// 		{
-	// 			std_in = open(argv[1], O_RDONLY);
-	// 			dup2(std_in, 0);
-	// 			exec_path(argv[2], env);
-	// 			close(0);	
-	// 		}
-	// 	}
-	// 	else 
-	// 	{
-	// 		std_out = open(argv[4], O_WRONLY | O_CREAT, 0644);
-	// 		dup2(std_out, 1);
-	// 		exec_path(argv[3], env);
-	// 		close(1);
-	// 	}
-	// pour la fonction pipe ls fd[1] est pour l'ecriture et le fd[0] pour la lecture}
