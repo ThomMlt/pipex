9c95a62ae9ff23e2c3637551d79eb4c82aca298f
Thomas <thomas@MacBook-Pro-de-Thomas.local>
2025-01-12 23:29:56 +0100

    commit

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: thomas <thomas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 17:04:40 by thomas            #+#    #+#             */
-/*   Updated: 2025/01/09 16:40:45 by thomas           ###   ########.fr       */
+/*   Updated: 2025/01/12 23:29:21 by thomas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,13 +28,13 @@ int	find_path(char **env)
 	return (-1);
 }
 
-void	exec_path(char **argv, char **env)
+void	exec_path(char *argv, char **env)
 {
 	char	**path;
 	char	**cmd;
 	int		i;
 
-	cmd = ft_split(argv[2], ' ');
+	cmd = ft_split(argv, ' ');
 	cmd[0] = ft_strjoin("/", cmd[0]);
 	path = ft_split(env[find_path(env)], ':');
 	i = 0;
@@ -49,26 +49,115 @@ void	exec_path(char **argv, char **env)
 	}
 }
 
-int	main(int argc, char **argv, char **env)
+void	processus_1(char *in_file, char *cmd, int pipefd[2], char **env)
 {
-	// if (argc > 4)
-	// {
-	// 	char **tab = ft_split(argv[2], ' ');
-	// 	while ( != NULL)
-	// 	{
-	// 		if (access(argv[1], F_OK) == -1)
-	// 			ft_printf("No such file or directory : %s", argv[1]);
-	// 		ft_printf("%d", execve(tab[0], tab, env));
-	// 		i++;
-	// 	}
-	// }
-	int	std_in;
-	std_in = open(argv[1], O_RDONLY);
-	dup2(std_in, 0);
-	(void)argc;
-	exec_path(argv, env);
+	int	in_file_fd;
+	
+	in_file_fd = open(in_file, O_RDONLY);
+	if (in_file_fd == -1)
+	{
+		perror("Error during opening the entry file");
+		exit(1);
+	}
+	close(pipefd[0]);
+	dup2(in_file_fd, 0);
+	dup2(pipefd[1], 1);
+	exec_path(cmd, env);
 }
 
-/* donc faire l'executable avec execve faire un str join avec la premiere parti de la commande
-sur le split des commande et savoir si on a acces a ce dossier et qu'on est la version X et qu'il
-existe*/
+void	processus_2(char *out_file, char *cmd, int pipefd[2], char **env)
+{
+	int	out_file_fd;
+
+	out_file_fd = open(out_file, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+	if (out_file_fd == -1)
+		perror("Error during opening the entry file");
+	close(pipefd[1]);
+	dup2(pipefd[0], 0);
+	dup2(out_file_fd, 1);
+	exec_path(cmd, env);
+}
+
+int	main(int argc, char **argv, char **env)
+{
+	int		pipefd[2];
+	pid_t	pid1;
+	pid_t	pid2;
+
+	if (argc != 5)
+		perror("Entry file1 cmd1 cmd2 file2");
+	if (pipe(pipefd) == -1)
+		perror("Error to pipe");
+	pid1 = fork();
+	if (pid1 == -1)
+        perror("Error to fork");
+	else if (pid1 == 0)
+		processus_1(argv[1], argv[2], pipefd, env);
+	pid2 = fork();
+	if (pid2 == -1)
+        perror("Error to fork");
+	else if (pid2 == 0)
+		processus_2(argv[4], argv[3], pipefd, env);
+	close(pipefd[0]);
+    close(pipefd[1]);
+    waitpid(pid1, NULL, 0);
+    waitpid(pid2, NULL, 0);
+}
+
+	// int		std_in = 0;
+	// int 	std_out = 0;
+	// int		pipefd[2];
+	// pid_t	pid1;
+	// pid_t	pid2;
+	
+	// if (argc != 5)
+	// 	exit(1);
+	// pid1 = fork();
+	// pid2 = fork();
+	// pipefd[0] = std_in;
+	// pipefd[1] = std_out;
+	// pipe(pipefd);
+	// if (pid1 == 0)
+	// {
+	// 	std_in = open(argv[1], O_RDONLY);
+	// 	dup2(std_in, 0);
+	// 	dup2(pipefd[1], 1);
+	// 	exec_path(argv[2], env);
+	// 	close(pipefd[0]);
+	// 	close(std_in);
+	// }
+	// if (pid2 == 0)
+	// {
+	// 	std_out = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	// 	dup2(pipefd[0], 0);
+	// 	dup2(std_out, 1);
+	// 	close(pipefd[1]);
+    //     close(std_out);
+    //     exec_path(argv[3], env);
+	// }
+	// close(pipefd[0]);
+    // close(pipefd[1]);
+    // waitpid(pid1, NULL, 0);
+    // waitpid(pid2, NULL, 0);
+
+	
+	// if (argc > 4)
+	// {
+	// 	if (pid == 0)
+	// 	{
+	// 		if (access(argv[1], F_OK) == 0)
+	// 		{
+	// 			std_in = open(argv[1], O_RDONLY);
+	// 			dup2(std_in, 0);
+	// 			exec_path(argv[2], env);
+	// 			close(0);	
+	// 		}
+	// 	}
+	// 	else 
+	// 	{
+	// 		std_out = open(argv[4], O_WRONLY | O_CREAT, 0644);
+	// 		dup2(std_out, 1);
+	// 		exec_path(argv[3], env);
+	// 		close(1);
+	// 	}
+	// pour la fonction pipe ls fd[1] est pour l'ecriture et le fd[0] pour la lecture}
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: thomas <thomas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/21 11:16:39 by tmillot           #+#    #+#             */
-/*   Updated: 2025/01/09 16:40:09 by thomas           ###   ########.fr       */
+/*   Updated: 2025/01/11 12:04:13 by thomas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # include "./ft_printf/ft_printf.h"
 # include <stdio.h>
 # include <unistd.h>
+# include <stdlib.h>
 # include <sys/types.h>
 # include <sys/stat.h>
 # include <fcntl.h>
